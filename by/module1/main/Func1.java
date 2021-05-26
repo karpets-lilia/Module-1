dc1c9adeef9bac3a61b50f263fa01026c9923ead
Lily Karpets <zoloto_lilia@icloud.com>
2021-05-26 18:25:42 +0300

    data has been corrected

--- by/module1/main/Func1.java
+++ by/module1/main/Func1.java
@@ -2,9 +2,9 @@ package by.module1.main;
 public class Func1 {
     /*Найдите значение функции : z = ((a=3)*b/2)+c  */
     public static void main(String args[]){
-        double a = 2;
-        double b = 3;
-        double c = 1;
+        double a = 4;
+        double b = 5;
+        double c = 6;
 
         double z = ((a - 3) * b/2) + c;
         System.out.println("При a = " + a + "b = " + b + "c = " + c + "значение функции z  =" + z);
